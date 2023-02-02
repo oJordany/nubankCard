fb3ad8a202023d058c7b47dc8c69d766e65cfb54
Jordany <jordanyluiz@gmail.com>
2023-02-02 08:40:40 -0300

    test: mouseover event in metalic effect

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const activator = document.querySelector("div.flip-card-inner")
 const frontCard = document.querySelector("div.card")
+const backCard = document.querySelector("div.card-back")
+
+frontCard.addEventListener("mouseover", e => {
+    document.styleSheets[0].cssRules[7].style.content = null
+})
 
 frontCard.addEventListener("click", e => {
     activator.style.transform = "rotateY(180deg)"
 })
-const backCard = document.querySelector("div.card-back")
+
 backCard.addEventListener("click", e => {
     activator.style.transform = "rotateY(360deg)"
 })
